ad1ec9da10228210cb970260e1365f8698bc4567
Kevin Fang <kfang@wencomine.com>
2022-03-06 19:26:18 -0800

    modify test.sh

--- test.sh
+++ test.sh
@@ -3,12 +3,11 @@
 URL="https://red-coast-05a8c4e10.1.azurestaticapps.net"
 
 response=$(curl -s -w "%{http_code}" $URL)
-http_code=$(tail -n1 <<< "$response")
 
-echo "return code is $http_code"
+echo "return code is $response"
 
 #exit script gracefully if http return code is successful.  Otherwise exit error
-if [[ "$http_code" == *"200"* ]]
+if [[ "$response" == *"200"* ]]
 then
         echo "passed"
 else

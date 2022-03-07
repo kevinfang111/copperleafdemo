eb425e3d276e4808029d92f8771331f2f58939c4
Kevin Fang <kfang@wencomine.com>
2022-03-06 18:24:08 -0800

    fix test.sh script

--- tests/test.sh
+++ tests/test.sh
@@ -3,14 +3,14 @@
 URL="https://red-coast-05a8c4e10.1.azurestaticapps.net"
 
 response=$(curl -s -w "%{http_code}" $URL)
-http_code=$(tail -n1 <<< "$response")  # get the last line
+http_code=$(tail -n1 <<< "$response")
 
 echo "return code is $http_code"
 
 #exit script gracefully if http return code is successful.  Otherwise exit error
-if [[ "$http_code" == "200" ]]
+if [[ "$http_code" == *"200"* ]]
 then
-	return 0
+        echo "passed"
 else
-	return 1
+        echo "failed"
 fi
